37f3a045b6c1078ab79d85aed97c98f89a12f764
Stephenice <stephencode@hotmail.com>
2019-12-16 14:43:46 +0100

    Added time to the page

---
+++ c01/add-contents.js
@@ -0,0 +1,16 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18){
+   greeting = " Good evening"; 
+}else if(hourNow > 12){
+    greeting = "good aftermoon";
+}else if(hourNow >0 ){
+
+    greeting = "good morning";
+}else{
+    greeting ="Welcome";
+}
+
+document.write('<h3>' + greeting + '</h3>');
